22ddc0eaa16fd9849056493bfda333b5a1c186d6
JosephWong123 <jjwong3@uci.edu>
2020-06-26 16:26:11 -0700

    last stable state, added a few event listeners for client

--- client/src/client.js
+++ client/src/client.js
@@ -1,3 +1,8 @@
+const showGameId = (id) => {
+    var displayGameId = document.createTextNode("Your game id is: " + id);
+    document.getElementById("displayId").appendChild(displayGameId);
+};
+
 const onFormSubmitted = (e) => {
     e.preventDefault();
     const input = document.querySelector('#chat');
@@ -5,25 +10,46 @@ const onFormSubmitted = (e) => {
     input.value = '';
 
     socket.emit('message', text);
-}
+};
 
 const socket = io();
 
-socket.on('gameCreated', (id) => {
-    gameId = id;
-    console.log(gameId);
-});
-
+let clientId = null;
 // HTML elements
-let clientId = socket.id;
+socket.on('connect', () => {
+    clientId = socket.id;
+})
+
 let gameId = null;
 
+const createOption = document.getElementById("createOption");
+const joinOption = document.getElementById("joinOption")
 const createButton = document.getElementById("create");
 const joinButton = document.getElementById("join");
 const gameIdText = document.getElementById("gameId");
 const playerName = document.getElementById("playerName");
 
 // Event handling
+createOption.addEventListener("click", e => {
+    for (let el of document.querySelectorAll(".joinForm")) {
+        el.style.display = "none";
+    }
+
+    for (let el of document.querySelectorAll(".createForm")) {
+        el.style.display = "block";
+    }
+});
+
+joinOption.addEventListener("click", e => {
+    for (let el of document.querySelectorAll(".createForm")) {
+        el.style.display = "none";
+    }
+
+    for (let el of document.querySelectorAll(".joinForm")) {
+        el.style.display = "block";
+    }
+});
+
 createButton.addEventListener("click", e => {
     const payLoad = {
         "clientId": clientId,
@@ -31,19 +57,41 @@ createButton.addEventListener("click", e => {
     }; 
     playerName.value = '';
     socket.emit('create', payLoad);
+
+    document.querySelector(".intro-wrapper").style.display = "none";
+    document.querySelector(".lobby").style.display = "block";
 });
 
 joinButton.addEventListener("click", e => {
+    gameId = gameIdText.value;
     const payLoad = {
         "clientId": clientId,
-        "gameId": gameIdText.value,
+        "gameId": gameId,
         "name": playerName.value
     };
 
     playerName.value = '';
     gameIdText.value = '';
     socket.emit('join', payLoad);
+});
+
+// Server events
+socket.on('gameCreated', (id) => {
+    gameId = id;
+    showGameId(gameId);
+});
+
+socket.on('gameJoined', () => {
+    document.querySelector(".intro-wrapper").style.display = "none";
+    document.querySelector(".lobby").style.display = "block";
+    showGameId(gameId);
 })
 
+socket.on('gameJoinError', () => {
+    var errorMsg = document.createTextNode("Error: Game ID " + gameId + " was not found.");
+    document.querySelector(".joinError").appendChild(errorMsg);
+});
 
-// document.querySelector('#chat-form').addEventListener('submit', onFormSubmitted);
+socket.on('playerJoined', () => {
+
+});
--- server/server.js
+++ server/server.js
@@ -13,24 +13,57 @@ const server = http.createServer(app);
 
 const io = socketio(server);
 
-io.on('connection', (socket) => {
-    console.log("Someone has connected!");
-    // console.log(socket);
-    socket.emit('message', 'You are connected!');
+const games = {};
 
+io.on('connection', (socket) => {
     socket.on('message', (text) => {
         io.emit('message', text);
     });
 
     socket.on('create', (payload) => {
         const clientId = payload.clientId;
+        const name = payload.name;
         const gameId = guid();
 
-        io.to(payload.socket).emit('gameCreated', gameId);
+        games[gameId] = {
+            "id": gameId,
+            "clients": [],
+            "admin": clientId,
+            "adminName": name
+        };
+        
+        io.to(payload.clientId).emit('gameCreated', gameId);
+
+        const game = games[gameId];
+        game.clients.push({
+            "clientId": clientId,
+            "name": name,
+        })
 
     });
 
     socket.on('join', (payload) => {
+        const clientId = payload.clientId;
+        const name = payload.name;
+        const gameId = payload.gameId;
+
+        if (!(gameId in games)) {
+            io.to(payload.clientId).emit('gameJoinError', gameId + " does not exist");
+            return;
+        }
+        const game = games[gameId];
+
+        // Max players = 8
+        if (game.clients.length >= 8) {
+            return;
+        }
+
+        io.to(payload.clientId).emit("gameJoined");
+
+        game.clients.push({
+            "clientId": clientId,
+            "name": name,
+        });
     });
 });
 
@@ -42,7 +75,7 @@ server.listen(8080, () => {
     console.log('RPS started on 8080');
 });
 
-// Generating gamde ID, definitely not copy/pasted from Stack Overflow
+// Generating game ID, definitely not copy/pasted from Stack Overflow
 function S4() {
     return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
 }
